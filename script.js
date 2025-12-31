ece7340b2e65ac206c4375393a1dffe682f4d8e0
shalanubale <ubaleshalan1836@gmail.com>
2025-12-31 18:39:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,3 @@ function firstChar(str) {
   }
   return '';
 }
-// Do not change the code below
-//Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
-alert(firstChar(text));
